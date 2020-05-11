18a44fefae5308eae6496dd23f0dcbde2f059537
feezeee <45011033+feezeee@users.noreply.github.com>
2020-05-12 00:18:04 +0300

    Добавил кнопку с смс боксом

--- Share Data/Form1.cs
+++ Share Data/Form1.cs
@@ -16,5 +16,10 @@ namespace Share_Data
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
