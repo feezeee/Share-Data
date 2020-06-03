08602da19481032a9151dd5c818a1db98e35afe5
snovis <sergeygoodgood@gmail.com>
2020-06-03 15:31:27 +0300

    добавил класс для возможности конекта между двумя устройствами

---
+++ Interface/Connection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Interface
+{
+    class Connection
+    {
+        const int localPort = 8010;
+        const int remotePort = 8010;
+        //static IPAddress BroadcustingAddress = IPAddress.Parse("224.0.0.251");
+        static IPAddress BroadcustingAddress = IPAddress.Parse("224.0.0.2");
+        //$netsh interface ip delete arpcache команда для очистки таблицы arp
+        //static IPAddress BroadcustingAddress = IPAddress.Parse("226.0.0.22");
+
+        const string m= "";
+        public static void SendBroadcastOfferToConnect()
+        {
+            UdpClient client = new UdpClient();
+            client.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(BroadcustingAddress, remotePort);
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, remotePort);
+           
+            try
+            {
+                while (true)
+                {
+                    string message = "offer to connect form sergey";
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    client.Send(data, data.Length, endPoint);
+                }
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+        public static void ReciveBroadcastOffer()
+        {
+            UdpClient receiver = new UdpClient(localPort); // UdpClient для получения данных
+            //receiver.MulticastLoopback = false;
+            receiver.JoinMulticastGroup(BroadcustingAddress);
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, remotePort);
+
+
+            IPEndPoint remoteIp = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+                    string message = Encoding.Unicode.GetString(data);
+                    Console.WriteLine(message+ " " + remoteIp.Address.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        public static void GetConnect()
+        {
+            Thread receiveThread = new Thread(new ThreadStart(ReciveBroadcastOffer));
+            receiveThread.Start();
+            SendBroadcastOfferToConnect(); // отправляем сообщение
+        }
+    }
+}
---
+++ Interface/ConnectionV2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Interface
+{
+    class ConnectionV2
+    {
+        const int localPort = 8010; // порт для приема информации
+        const int remotePort = 8010; // порт для отправки информации
+        // данный адрес необходимо будет получать, ибо он может варироваться, но пока с кастылем
+        static string broadcastingIP = "192.168.1.255"; // широковещательный адрес локальной сети
+
+        static void SendBroadcastOfferToConnect() // функция которая отправлет широковещательное сообщение 
+        {
+            while(true)
+            {
+                // создаем соект для работы по пратоколу UDP, в сети Internet, для передачи дейтаграмных сообщений
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);  
+
+                IPAddress broadcast = IPAddress.Parse(broadcastingIP);
+
+                byte[] sendbuf = Encoding.ASCII.GetBytes("offer to conect");// кодируем сообщение из строки в битовый массив
+                IPEndPoint ep = new IPEndPoint(broadcast, remotePort);// создаем полыный адрес получателя, тоесть добавляем к IP еще и прот
+
+                s.SendTo(sendbuf, ep);// отправлем сообщение на адрес получателя
+                Console.WriteLine("Message sent to the broadcast address");
+
+                for (int i = 0; i < 100000000; i++) ; // задержка между рассылкой сообщение калхоз вариант
+            }
+        }
+        static void ReciveBroadcastOffer()
+        {
+            UdpClient listener = new UdpClient(localPort); // для прослушивания сообщений udp приходящих на локальный порт
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, localPort); // адрес приема, для приема всех сообщений
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP); // получаем сообщение
+
+                    Console.WriteLine($"Received broadcast from {groupEP.Address} :"); // вывод адреса атправителя
+                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}"); //вывод соообщения атправителя, предвор дешифр
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        public static void GetConnect() // класс интерфейс
+        {
+            Thread receiveThread = new Thread(new ThreadStart(ReciveBroadcastOffer)); //созадем новый поток отдельно для получения
+            receiveThread.Start(); // начинаем слушать сеть
+            SendBroadcastOfferToConnect(); // запускаем процесс отправки сообщений
+        }
+    }
+}
