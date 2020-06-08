f5e008968f464460f35127de05cd80c6619f19e6
feezeee <45011033+feezeee@users.noreply.github.com>
2020-06-08 22:55:45 +0300

    Добавил getting_name

---
+++ Share Data/getting_name.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share_Data
+{
+    class getting_name
+    {
+        public static string get_name()
+        {
+            string name;
+
+            try 
+            {
+                name = System.Net.Dns.GetHostName();
+
+            }
+            catch 
+            {
+                //Создание объекта для генерации чисел
+                Random rnd = new Random();
+
+                //Получить случайное число (в диапазоне от 0 до 10)
+                int value = rnd.Next(100000000, 999999999);
+                try
+                {
+                    name = System.Environment.UserName + value.ToString();
+
+                }
+                catch
+                {
+                    name = "User" + value.ToString();
+                }
+            }
+
+            return name;
+        }
+    }
+}
