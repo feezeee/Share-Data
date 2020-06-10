c5ca3111d6043535537e02c3f57eccefe172ec86
snovis <sergeygoodgood@gmail.com>
2020-06-10 16:46:11 +0300

    c

---
+++ test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread sendThread = new Thread(new ThreadStart(ConnectionV2.SendBroadcastOfferToConnect)); //созадем новый поток отдельно для получения
+            sendThread.IsBackground = true;
+            sendThread.Start(); // запускаем процесс отправки сообщений
+        }
+    }
+}
