96d37b648c4d7d7c45c07dfa5205c444c74a7454
feezeee <45011033+feezeee@users.noreply.github.com>
2020-06-21 22:26:02 +0300

    xaml components

---
+++ ConnectedForm/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectedForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Height00 = 100;
+            Width00 = 100;
+        }
+
+        private int _height00;
+        public int Height00
+        {
+            get { return _height00; }
+            set { _height00 = value; }
+        }
+
+        private int _height01;
+        public int Height01
+        {
+            get { return _height01; }
+            set { _height01 = value; }
+        }
+
+        private int _height10;
+        public int Height10
+        {
+            get { return _height10; }
+            set { _height10 = value; }
+        }
+
+        private int _height11;
+        public int Height11
+        {
+            get { return _height11; }
+            set { _height11 = value; }
+        }
+
+        /////////////////////Ширина
+        ///
+        private int _width00;
+        public int Width00
+        {
+            get { return _width00; }
+            set { _width00 = value; }
+        }///
+        private int _width01;
+        public int Width01
+        {
+            get { return _width01; }
+            set { _width01 = value; }
+        }
+        ///
+        private int _width10;
+        public int Width10
+        {
+            get { return _width10; }
+            set { _width10 = value; }
+        }
+        ///
+        private int _width11;
+        public int Width11
+        {
+            get { return _width11; }
+            set { _width11 = value; }
+        }
+    }
+}
---
+++ interactDomain/UserControl1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.MobileControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace interactDomain
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        public List<string> list = new List<string>();
+
+        public class files
+        {
+            public string nameFile { get; set; }
+
+            public string time { get; set; }
+
+            public string sizeFile { get; set; }
+        }
+
+        public UserControl1()
+        {
+
+            setHeight = 1000;
+            setWidth = 1000;
+            InitializeComponent();
+            setHeight = 1000;
+            setWidth = 1000;
+            //DataContext = this;
+            //loadInfromationAboutFiles("1", "10", "100");
+
+            //loadInfromationAboutFiles("2", "20", "200");
+
+            //loadInfromationAboutFiles("3", "30", "200");
+        }
+
+        private int _width;
+
+        public int setWidth
+        { 
+            get {   return _width;   }
+            set {   _width = value;  }
+        }
+
+        private int _height;
+
+        public int setHeight
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public void loadInfromationAboutFiles(string nameFile, string time,string sizeFile)
+        {
+            files dataFile = new files() // создаём экземпляр класса        
+            {
+                nameFile = nameFile, // указываем имя файла  
+                time = time, // указываем время создания    
+                sizeFile = sizeFile // указываем пароль  
+            };
+            listUsers.Items.Add(dataFile); // выводим строку в список 
+            
+        }
+
+    }
+   
+}
