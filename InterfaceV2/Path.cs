fe0e20384219ea683e2c04aff3662b3c8611e024
snovis <sergeygoodgood@gmail.com>
2020-06-24 09:55:37 +0300

    11

---
+++ InterfaceV2/Path.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceV2
+{
+    public class Path
+    {
+        private List<string> History;
+        public static string StartPath = ".";
+
+        private string path = ".";
+
+        public string GoToTheDirectory(string direcotryName, string directoryLocaltion = null)
+        {
+            if (directoryLocaltion == null) directoryLocaltion = path;
+
+            if (directoryLocaltion == ".")
+            {
+                directoryLocaltion = "";
+            }
+            directoryLocaltion += '\\' + direcotryName;
+            History.Add(directoryLocaltion);
+            return directoryLocaltion;
+        }
+    }
+}
